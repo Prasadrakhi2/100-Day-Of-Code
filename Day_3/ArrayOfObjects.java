fae035e3c8d690746965e26fb42efe2cfc5338a0
rakhi <prasadrakhi20004@gmail.com>
2025-09-22 23:51:35 +0530

    array of object

--- Day_3/ArrayOfObjects.java
+++ Day_3/ArrayOfObjects.java
@@ -17,8 +17,8 @@ public class ArrayOfObjects{
         s2.marks = 65;
 
         Students s3 = new Students();
-        s3.rollno = 2;
-        s3.name = "riya";
+        s3.rollno = 3;
+        s3.name = "vidhi";
         s3.marks = 55;
 
         Students student[] = new Students[3];
@@ -26,8 +26,12 @@ public class ArrayOfObjects{
         student[1] = s2;
         student[2] = s3;
 
-        for(int i=0; i<student.length; i++){
-            System.out.println(student[i].name + ":" + student[i].rollno + " - " + student[i].marks);
+        // for(int i=0; i<student.length; i++){
+        //     System.out.println(student[i].name + ":" + student[i].rollno + " - " + student[i].marks);
+        // }
+
+        for(Students stud : student){
+             System.out.println(stud.name + ":" + stud.rollno + " - " + stud.marks);
         }
     }
 }
