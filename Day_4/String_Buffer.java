a457c95ecbe9e16bdc2d8881fe367b35628c755c
rakhi <prasadrakhi20004@gmail.com>
2025-09-23 23:37:03 +0530

    stringbuffer

---
+++ Day_4/String_Buffer.java
@@ -0,0 +1,8 @@
+public class String_Buffer {
+    public static void main(String args[]){
+        StringBuffer sb = new StringBuffer("rakhi");
+        sb.append(" prasad");
+        sb.insert( 6, "java ");
+        System.out.println(sb);
+    }    
+}
