c592bbb2f342b2be63ce1bb69ac10a5069a4f7c7
rakhi <prasadrakhi20004@gmail.com>
2025-09-25 23:16:10 +0530

    static block & variable

---
+++ Day_5/StaticKeywords.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Mobile{
+    String name;
+    int price;
+    static String product;
+
+    
+    public void show(){
+        // System.err.println(name + " : " + price + " : " + product);
+        System.err.println("in instance block");
+    }
+
+    static{
+        product = "phone";
+        System.out.println("in static block");
+    }
+
+}
+
+public class StaticKeywords{
+    public static void main(String args[]){
+        Mobile obj1 = new Mobile();
+        obj1.name = "apple";
+        obj1.price = 1500;
+        obj1.product = "phone";
+
+        Mobile obj2 = new Mobile();
+        obj2.name = "samsung";
+        obj2.price = 1800;
+        obj2.product = "cell phone";
+
+        obj1.show();
+        obj2.show();
+    }
+}
