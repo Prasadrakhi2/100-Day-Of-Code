10c7d14add7c9ab7bb9217a57dd3446280cf828e
rakhi <prasadrakhi20004@gmail.com>
2025-09-30 23:08:06 +0530

    this method

---
+++ Day_7/ThisKey.java
@@ -0,0 +1,26 @@
+class A{
+    public A(){
+        System.out.println("in A");
+    }
+    public A(int n){
+        System.out.println("in int A");
+    }
+}
+
+class B extends A{
+    public B(){
+         
+        System.out.println("in B");
+    }
+    public B(int n){
+       this();
+        System.out.println("in int B");
+    }
+}
+
+public class ThisKey {
+    public static void main(String[] args) {
+         B obj = new B(5);
+    }
+    
+}
