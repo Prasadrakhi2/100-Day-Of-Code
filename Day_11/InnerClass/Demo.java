e98a8f4e925bdf43a2fba6b8a83550d865ce2014
rakhi <prasadrakhi20004@gmail.com>
2025-10-04 21:56:08 +0530

    inner class

---
+++ Day_11/InnerClass/Demo.java
@@ -0,0 +1,24 @@
+class A{
+    int a;
+    public void show(){
+        System.out.println("In show");
+    }
+
+    class B{
+    // static class B{
+        public void config(){
+        System.out.println("In config");
+    }
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        A.B obj1 = obj.new B();
+        // A.B obj1 = new A.B();
+        obj1.config();
+    }    
+}
