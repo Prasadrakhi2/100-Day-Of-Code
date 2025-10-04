97d89bdd4e49eb981efaf9eec5bf1f94090146d0
rakhi <prasadrakhi20004@gmail.com>
2025-10-04 22:04:39 +0530

    anonnymous inner class

---
+++ Day_11/AnonnymousInnerClass/Demo.java
@@ -0,0 +1,19 @@
+class A{
+    public void show(){
+        System.out.println("in A show");
+    }
+}
+
+
+
+public class Demo {
+    public static void main(String[] args) {
+        A obj = new A()
+        {
+            public void show(){
+            System.out.println("in new show");
+            }
+        };
+       obj.show();
+    }
+}
