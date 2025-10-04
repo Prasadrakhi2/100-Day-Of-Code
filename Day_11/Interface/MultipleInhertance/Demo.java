729e90040f9946a9e372d43df50a4f6831fb1582
rakhi <prasadrakhi20004@gmail.com>
2025-10-04 22:51:43 +0530

    multiple inheritance

---
+++ Day_11/Interface/MultipleInhertance/Demo.java
@@ -0,0 +1,30 @@
+interface A{
+    int a = 10;   // final / static variable
+    String area = "Mumbai";
+     void show();
+     void config();
+}
+
+interface C{
+    void singing();
+}
+class B implements A,C{
+    public void show(){
+        System.out.println("in show");
+    }
+    public void config(){
+        System.out.println("in config");
+    }
+    public void singing(){
+        System.out.println("singing...");
+    }
+}
+
+public class Demo{
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+        obj.config();
+        obj.singing();
+    }
+}
